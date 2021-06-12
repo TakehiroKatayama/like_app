a0cebe7a1d23090c20ad824dd5b411cacbdcfc11
TakehiroKatayama <t.katayama.info@gmail.com>
2021-06-12 11:37:53 +0900

    詳細ページにいいねの表示を作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+  # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
